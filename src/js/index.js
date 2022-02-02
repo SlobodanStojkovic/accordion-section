20e2f34760b95e9a678131b3425449bbd13382a4
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-02-02 12:23:07 +0100

    Fix accordian javascript functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,24 @@
-const elements = document.querySelectorAll(".faq__section__qa__question");
+const elements = document.querySelectorAll(".faq__section__qa");
 
 elements.forEach((element) => {
   let btn = element.querySelector(".faq__section__qa__question__button");
   let icon = element.querySelector(".faq__section__qa__question button i");
   var answer = element.lastElementChild;
-  var answers = document.querySelectorAll(
-    ".faq__section__qa .faq__section__qa__answer"
-  );
-  console.log(answers);
+  var answers = document.querySelectorAll(".faq__section__qa__answer");
 
   btn.addEventListener("click", () => {
     answers.forEach((ans) => {
       let ansIcon = ans.parentElement.querySelector("button i");
-      console.log(ans);
+      console.log(ansIcon.className);
       if (answer !== ans) {
-        ans.classList.add("active");
-        ansIcon.className = "fas fa-chevron-up";
+        ans.classList.add("inactive");
+        ansIcon.className = "fas fa-chevron-down";
       }
     });
 
     answer.classList.toggle("inactive");
     icon.className === "fas fa-chevron-up"
-      ? (icon.className = "fas fa-angle-down")
+      ? (icon.className = "fas fa-chevron-down")
       : (icon.className = "fas fa-chevron-up");
   });
 });
