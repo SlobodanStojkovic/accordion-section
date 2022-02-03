eadc05dc46f24c8c52a936e2b5928a54f3c0759b
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-02-03 09:36:23 +0100

    Fix hover effect added and only one element can be active at a time

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,18 @@
 const elements = document.querySelectorAll(".faq__section__qa");
 
 elements.forEach((element) => {
-  let btn = element.querySelector(".faq__section__qa__question__button");
+  let question = element.querySelector(".faq__section__qa__question");
   let icon = element.querySelector(".faq__section__qa__question button i");
   var answer = element.lastElementChild;
   var answers = document.querySelectorAll(".faq__section__qa__answer");
 
-  btn.addEventListener("click", () => {
+  question.addEventListener("click", () => {
     answers.forEach((ans) => {
       let ansIcon = ans.parentElement.querySelector("button i");
+      ans.parentElement.classList.remove("clicked");
       if (answer !== ans) {
-        ans.parentElement.classList.remove("clicked");
         ansIcon.className = "fas fa-chevron-down";
+        ans.classList.add("inactive");
       }
       if (answer === ans) {
         ans.parentElement.classList.add("clicked");
