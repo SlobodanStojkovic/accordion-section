0bd36da2c87f981d4f9b57b90134ddf8e9f59843
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-02-14 18:00:10 +0100

    Change code style improvements, removed excessive html, arrow added with css and removed as image

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,41 @@
-const elements = document.querySelectorAll(".faq__section__qa");
+const faqQuestion = document.querySelectorAll(".faq__section__qa__question");
 
-elements.forEach((element) => {
-  let question = element.querySelector(".faq__section__qa__question");
-  let icon = element.querySelector(".faq__section__qa__question button i");
-  var answer = element.lastElementChild;
-  var answers = document.querySelectorAll(".faq__section__qa__answer");
-
+faqQuestion.forEach((question) => {
   question.addEventListener("click", () => {
-    answers.forEach((ans) => {
-      let ansIcon = ans.parentElement.querySelector("button i");
-      ans.parentElement.classList.remove("clicked");
-      if (answer !== ans) {
-        ansIcon.className = "fas fa-chevron-down";
-        ans.classList.add("inactive");
-      }
-      if (answer === ans) {
-        ans.parentElement.classList.add("clicked");
-      }
+    let faqMainWrapper = question.closest(".faq__section__text"),
+      faqItems = faqMainWrapper.children,
+      faqCurrentItem = question.parentElement,
+      faqCurrentAnswear = faqCurrentItem.lastElementChild;
+    console.log(faqCurrentAnswear);
+    faqCurrentItem.classList.toggle("active");
+
+    if (faqCurrentAnswear.style.maxHeight) {
+      faqCurrentAnswear.style.maxHeight = null;
+    } else {
+      faqCurrentAnswear.style.maxHeight = faqCurrentAnswear.scrollHeight + "px";
+    }
+
+    let otherFaqItems = [...faqItems].filter((item) => {
+      return item !== faqCurrentItem;
     });
 
-    answer.classList.toggle("inactive");
-    icon.className === "fas fa-chevron-up"
-      ? (icon.className = "fas fa-chevron-down")
-      : (icon.className = "fas fa-chevron-up");
+    otherFaqItems.forEach((item) => {
+      item.classList.remove("active");
+      item.lastElementChild.style.maxHeight = null;
+    });
+
+    let arrows = document.querySelectorAll(".faq__section__qa__question");
+
+    arrows.forEach((item) => {
+      if (item.parentElement.classList.contains("active")) {
+        item.classList.remove("down");
+        item.classList.add("up");
+      }
+
+      if (!item.parentElement.classList.contains("active")) {
+        item.classList.add("down");
+        item.classList.remove("up");
+      }
+    });
   });
 });
